46bb99a34e1863d939ee9ab346a07a1e66080232
profeLeguizaNicolas <136519391+profeLeguizaNicolas@users.noreply.github.com>
2025-10-29 21:09:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+let boton = document.getElementById('boton');
+let puntos = 0;
+
+boton.addEventListener('click',()=>{
+    let usuario = document.getElementById('nom').value;
+    
+    let valores = JSON.parse(document.getElementsByClassName("true"));
+    let nombre = document.getElementById('bruce').checked;
+    let mayor = document.getElementById('alfred').checked;
+    let ciudad = document.getElementById('gotica').checked;
+    let robin = document.getElementById('di').checked;
+    let joker = document.getElementById('guason').checked;
+    let movil = document.getElementById('batmobile').checked;
+    let lucius = document.getElementById('luci').checked;
+    let muerte = document.getElementById('muerte').checked;
+    let lex = document.getElementById('lex').checked;
+    let noche = document.getElementById('noche').checked;
+    for(let i = 0; i < valores.length - 1 ; i++){console.log(valores[i].checked)}
+localStorage.setItem("valores",JSON.stringify(valores));
+    if(nombre) puntos++
+    if(mayor) puntos++
+    if(ciudad) puntos++
+    if(robin) puntos++
+    if(joker) puntos++
+    if(movil) puntos++
+    if(lucius) puntos++
+    if(muerte) puntos++
+    if(lex) puntos++
+    if(noche) puntos++
+
+ 
+    let resultados = JSON.parse(localStorage.getItem("resultados"))||[]
+
+    resultados.push({usuario,puntos});
+    localStorage.setItem("resultados",JSON.stringify(resultados))
+    window.location.href = "fin.html";
+
+   
+});
+
---
+++ main2.js
@@ -0,0 +1,35 @@
+let punto = localStorage.getItem("point");
+let point = document.getElementById("puntos");
+point.textContent = punto;
+
+let usuario = localStorage.getItem("usuario");
+let body = document.body;
+
+let resultados = JSON.parse(localStorage.getItem("resultados"))
+
+
+let existente = resultados.find(r => r.usuario === usuario);
+if (existente) {
+  existente.punto = punto; // actualiza puntaje
+} else {
+  resultados.push({ usuario, punto });
+}
+
+
+let valores = JSON.parse(localStorage.getItem("valores"));
+
+
+console.log(valores);
+
+let tabla = `<style>td,th{border: 2px red solid; padding:10px;}</style><h1 style="text-align:center">Tabla de Puntajes</h1> <br> <table style="margin:0 auto; border-collapse:collapse;"><thead><th>Nombre</th><th>Puntos</th></thead><tbody>`;
+    for(let i = 0 ; i < resultados.length-1;i++){
+        tabla += `<tr><td>${resultados[i].usuario}</td><td>${resultados[i].puntos}</td></tr>`
+    }
+tabla += `</tbody></table>`
+
+body.innerHTML += tabla;
+
+
+
+
+document.getElementById("reset").addEventListener("click",()=>{localStorage.clear()})
